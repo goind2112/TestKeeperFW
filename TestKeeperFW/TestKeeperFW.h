4831bce0f6c41b735111bc7a9144372829ed1801
goind2112 <110747378+goind2112@users.noreply.github.com>
2023-07-28 07:05:17 +0600

    Initial Commit

---
+++ TestKeeperFW/TestKeeperFW.h
@@ -0,0 +1,18 @@
+//
+//  TestKeeperFW.h
+//  TestKeeperFW
+//
+//  Created by Даниил Мухсинятов on 28.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestKeeperFW.
+FOUNDATION_EXPORT double TestKeeperFWVersionNumber;
+
+//! Project version string for TestKeeperFW.
+FOUNDATION_EXPORT const unsigned char TestKeeperFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestKeeperFW/PublicHeader.h>
+
+
